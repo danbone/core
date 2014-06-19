ea33932a26176c0c98c7f98689e773e547769531
dan <dan@ubuntu.(none)>
2014-06-19 22:58:15 +0100

    modified:   riscv_ex.v new file:   riscv_functions.vh

--- rtl/riscv_ex.v
+++ rtl/riscv_ex.v
@@ -1,22 +1,60 @@
+`include "riscv_functions.vh"
+
 module riscv_ex (
-    input                   clk,
-    input                   rstn,
-    //ID input
-    input                   id_mem_funct,
-    input                   id_mem_sb,
-    //MEM output
-    output                  mem_result,
-    output                  mem_funct,
-    output                  mem_sb,
+    input                       clk,
+    input                       rstn,
     //Downstream
-    input                   ds_rdy,
-    output                  ds_ack,
+    input                       ds_rdy,
+    output                      ds_ack,
+    //ID input
+    input  [`EX_FUNCT_W-1:0]    id_ex_funct,
+    input  [31:0]               id_ex_op1,
+    input  [31:0]               id_ex_op2,
+    input  [`MEM_FUNCT_W-1:0]   id_ex_mem_funct,
+    input  [31:0]               id_ex_mem_data,
+    input  [4:0]                id_ex_wb_rsd,
     //Upstream 
-    input                   us_rdy,
-    output                  us_ack
+    input                       us_rdy,
+    output                      us_ack,
+    //MEM output
+    output [31:0]               ex_mem_result,
+    output [`MEM_FUNCT_W-1:0]   ex_mem_funct,
+    output [31:0]               ex_mem_data,
+    output [4:0]                ex_mem_wb_rsd
 );
 
-reg                         us_rdy_reg;
+//Outputs
+reg                      us_rdy_reg;
+reg  [31:0]              ex_mem_result;
+reg  [`MEM_FUNCT_W-1:0]  ex_mem_funct;
+reg  [31:0]              ex_mem_data;
+reg  [4:0]               ex_mem_wb_rsd;
+
+wire                     flip_op2;
+wire [31:0]              neg_op2;
+wire [5:0]               shft_amnt;
+
+wire [31:0]              op2;
+
+assign neg_op2   = (~id_ex_op2) + 1;
+assign shft_amnt = id_ex_op2 & 'h1f;
+assign op2       = (flip_op2) ? neg_op2 : id_ex_op2;
+
+always @ (*) begin
+    case (id_ex_funct) 
+        `EX_ADD ,
+        `EX_SUB : result = id_ex_op1 + id_ex_op2;
+        `EX_OR  : result = id_ex_op1 | id_ex_op2;
+        `EX_XOR : result = id_ex_op1 ^ id_ex_op2;
+        `EX_AND : result = id_ex_op1 & id_ex_op2;
+        `EX_STL : result = ($signed(id_ex_op1) < $signed(id_ex_op2)) ?  1'b1 : 1'b0;
+        `EX_STLU: result = (id_ex_op1 < id_ex_op2) ? 1'b1 : 1'b0;
+        `EX_SLL : result = id_ex_op1 << shft_amnt;
+        `EX_SRL : result = id_ex_op1 >> shft_amnt;
+        `EX_SRA : result = id_ex_op1 >>> shft_amnt;
+        default : result = 32'b0;
+    endcase
+end
 
 //This module cannot stall
 assign ds_ack = us_rdy;
@@ -25,71 +63,86 @@ assign us_rdy = us_rdy_reg;
 always @ (posedge clk, negedge rstn) begin
     if (~rstn) begin
         us_rdy_reg     <= 1'b0;
-        mem_result_reg <= 'h0;
-        mem_funct_reg  <= 'h0;
-        mem_sb_reg     <= 'h0;
+        ex_mem_result  <= 'h0;
+        ex_mem_funct   <= `MEM_NOP;
+        ex_mem_data    <= 'h0;
+        ex_mem_wb_rsd  <= 'h0;
     end
     else begin
         if (us_rdy_reg && us_ack) begin
             us_rdy_reg     <= ds_rdy;
-            mem_result_reg <= mem_result_nxt;
-            mem_funct_reg  <= mem_funct_nxt;
-            mem_sb_reg     <= mem_sb_nxt;
+            ex_mem_result  <= result;
+            ex_mem_funct   <= id_ex_mem_funct;
+            ex_mem_data    <= id_ex_mem_data;
+            ex_mem_wb_rsd  <= id_ex_wb_rsd;
         end
     end
 end
 
-assign ex_mem_result = result_ff;
+endmodule
 
-localparam FUNCT_NOP_OH     = 10'b0000000000;
-localparam FUNCT_ADD_OH     = 10'b0000000001;
-localparam FUNCT_SUB_OH     = 10'b0000000010;
-localparam FUNCT_OR_OH      = 10'b0000000100;
-localparam FUNCT_XOR_OH     = 10'b0000001000;
-localparam FUNCT_AND_OH     = 10'b0000010000;
-localparam FUNCT_STL_OH     = 10'b0000100000;
-localparam FUNCT_STLU_OH    = 10'b0001000000;
-localparam FUNCT_SLL_OH     = 10'b0010000000;
-localparam FUNCT_SRL_OH     = 10'b0100000000;
-localparam FUNCT_SRA_OH     = 10'b1000000000;
+`ifdef EX_TB
+module tb ();
 
-wire [31:0] op1_st1;
-wire [31:0] op2_st1;
+reg tb_clk  = 1'b0;
+reg tb_rstn = 1'b0;
 
-wire [31:0] op1_st2;
-wire [31:0] op2_st2;
+//Generate stimulus
+always @ (posedge clk) begin
+end
 
-wire [31:0] result_nxt;
+//Check process
+always @ (posedge clk) begin
+    if (~tb_rstn) begin
+        check_fifo_flush <= 1'b1;
+    end
+    else begin
+        check_fifo_flush <= 1'b0;
+        check_fifo_read  <= 1'b0;
+        if (us_rdy && us_ack) begin
+            if (check_fifo_empty) begin
+                $display("ERROR: DUT output rdy but no data in check fifo");
+                $finish();
+            end
+            else begin
+                check_fifo_mem_funct  = check_fifo_rdata[`CHECK_FIFO_MEM_FUNCT_RANGE];
+                check_fifo_mem_data   = check_fifo_rdata[`CHECK_FIFO_MEM_DATA_RANGE ];
+                check_fifo_mem_result = check_fifo_rdata[`CHECK_FIFO_RESULT_RANGE   ];
+                check_fifo_wb_rsd     = check_fifo_rdata[`CHECK_FIFO_WB_RSD_RANGE   ];
+                check_fifo_trans_cnt  = check_fifo_rdata[`CHECK_FIFO_TRANS_CNT_RANGE];
 
-wire        flip_op2_st2;
-wire [31:0] neg_op2_st2;
-wire [5:0]  shft_amnt;
-
-assign op1_st1 = (id_ex_op1_cntl) ? id_ex_pc    : ct_ex_op1;
-assign op2_st1 = (id_ex_op2_cntl) ? id_ex_immed : ct_ex_op2;
-
-assign op1_st2 = op1_st1;
-assign op2_st2 = (flip_op2) ? neg_op2 : op2_st1;
-
-assign flip_op2 == (id_ex_funct == FUNCT_SUB_OH) ? 1'b1 : 1'b0;
-
-assign neg_op2 = (~op2_st1)+1;
-assign shft_amnt = op2_st1 & 'h1f;
-
-always @ (*) begin
-    case (id_ex_funct) 
-        FUNCT_ADD_OH ,
-        FUNCT_SUB_OH : result = op1_st2 + op2_st2;
-        FUNCT_OR_OH  : result = op1_st2 | op2_st2;
-        FUNCT_XOR_OH : result = op1_st2 ^ op2_st2;
-        FUNCT_AND_OH : result = op1_st2 & op2_st2;
-        FUNCT_STL_OH : result = ($signed(op1_st2) < $signed(op2_st2)) ? 1'b1 : 1'b0;
-        FUNCT_STLU_OH: result = (op1_st2 < op2_st2) ? 1'b1 : 1'b0;
-        FUNCT_SLL_OH : result = op1_st2 << shft_amnt;
-        FUNCT_SRL_OH : result = op1_st2 >> shft_amnt;
-        FUNCT_SRA_OH : result = op1_st2 >>> shft_amnt;
-        default      : result = 32'b0;
-    endcase
+                if (check_fifo_mem_funct != dut_mem_funct) begin
+                    $display("ERROR: dut_mem_funct mismatch for trans : %0d", 
+                        check_fifo_trans_cnt);
+                    check_fifo_mismatch |= 1;
+                end
+                if (check_fifo_mem_data != dut_mem_data) begin
+                    $display("ERROR: dut_mem_data mismatch for trans : %0d", 
+                        check_fifo_trans_cnt);
+                    check_fifo_mismatch |= 1;
+                end
+                if (check_fifo_mem_result != dut_mem_result) begin
+                    $display("ERROR: dut_mem_result mismatch for trans : %0d", 
+                        check_fifo_trans_cnt);
+                    check_fifo_mismatch |= 1;
+                end
+                if (check_fifo_wb_rsd != dut_wb_rsd) begin
+                    $display("ERROR: dut_wb_rsd mismatch for trans : %0d", 
+                        check_fifo_trans_cnt);
+                    check_fifo_mismatch |= 1;
+                end
+                if (check_fifo_mismatch) begin
+                    $display("Test ending due to mismatches");
+                    $finish();
+                end
+                else begin
+                    $display("Match for trans : %0d", check_fifo_trans_cnt);
+                    check_fifo_read <= 1'b1;
+                end
+            end
+        end
+    end
 end
 
 endmodule
+`endif
---
+++ rtl/riscv_functions.vh
@@ -0,0 +1,23 @@
+`define EX_FUNCT_W  10
+`define EX_NOP      10'b0000000000
+`define EX_ADD      10'b0000000001
+`define EX_SUB      10'b0000000010
+`define EX_OR       10'b0000000100
+`define EX_XOR      10'b0000001000
+`define EX_AND      10'b0000010000
+`define EX_STL      10'b0000100000
+`define EX_STLU     10'b0001000000
+`define EX_SLL      10'b0010000000
+`define EX_SRL      10'b0100000000
+`define EX_SRA      10'b1000000000
+
+`define MEM_FUNCT_W  8
+`define MEM_NOP      8'b00000000
+`define MEM_LB       8'b00000001
+`define MEM_LH       8'b00000010
+`define MEM_LW       8'b00000100
+`define MEM_LBU      8'b00001000
+`define MEM_LBH      8'b00010000
+`define MEM_SB       8'b00100000
+`define MEM_SH       8'b01000000
+`define MEM_SW       8'b10000000
